4534b8fbbf482a39947050eaa2c3b0e6dc1ba090
Madhav Sunil <madhavsuni98@gmail.com>
2018-10-19 00:11:13 +0530

    MainMenu completed,Credits and about takshak added

--- Assets/scripts/mainMenu.cs
+++ Assets/scripts/mainMenu.cs
@@ -2,12 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class mainMenu : MonoBehaviour {
-
+private Vector3 newTransfrom;
+public TextMeshProUGUI textMesh;
+public GameObject aboutMenu,Menu,optionsMenu;
 public void  PlayGame()
 {    
       
        SceneManager.LoadScene("game");
+
 }	
+void Awake(){
+     newTransfrom=textMesh.transform.position;
+}
+public void opstions(){
+    Menu.SetActive(false);
+    optionsMenu.SetActive(true);
+}
+public void About(){
+       textMesh.transform.position= newTransfrom;
+       Menu.SetActive(false);
+       aboutMenu.SetActive(true);
+       Invoke("back",55);    
+}
+public void back(){
+    aboutMenu.SetActive(false);
+    optionsMenu.SetActive(false);
+    Menu.SetActive(true);
+}
+
 }
